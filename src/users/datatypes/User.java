685d7a4be5b3e7929f84e09a8a4bbfc45cdccd7c
Jsevillamol <jsevillamol@outlook.com>
2016-05-17 16:51:36 +0200

    Add functionality to User

--- src/users/datatypes/User.java
+++ src/users/datatypes/User.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import sharedtypes.FilePath;
 
+/*
+ * Object which represents an user.
+ * They are built by IUserDAOs and have simple functionality allowing modification
+ * and checks.
+ */
 public class User {
 	String userId;
 	
@@ -21,4 +26,27 @@ public class User {
 	 * List of projects in which the user collaborates.
 	 */
 	List<FilePath> projects;
+	
+	/*
+	 * Returns true if the password corresponds to the stored password.
+	 */
+	public boolean checkPassword(String password){
+		return this.password.equals(password);
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public UserLevel getUserLevel() {
+		return userLevel;
+	}
+
+	public List<FilePath> getProjects() {
+		return projects;
+	}
+	
+	public void removeProject(FilePath project){
+		projects.remove(project);
+	}
 }
