a3dbe2566c70ad12d8caaa42ff1e55411e8a71d8
Jsevillamol <jsevillamol@outlook.com>
2016-05-20 12:38:39 +0200

    Add test infrastructure

---
+++ test/test/TestRunner.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import files.UserMainTest;
+
+/**
+ * Runs all tests in the test suite.
+ * @author Jaime
+ *
+ */
+public class TestRunner {
+   public static void main(final String[] args) {
+      final Result result = JUnitCore.runClasses(
+    		  //Test suite
+    		  UserMainTest.class
+    		  //Here go classes
+    		  );
+      for (final Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+      
+      System.out.println(
+    		  (result.wasSuccessful())
+    		  	?"Test successful!" 
+    		  	:"Test failed"
+    		  );
+   }
+}
